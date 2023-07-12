3f9d10e5c34de378e2832eba78413df6e38e7136
Denis Valladares <denisvalladares402@gmail.com>
2023-07-11 22:10:28 -0600

    add validation and handle exception of validation

--- src/test/java/com/example/sale/practice/controller/ExceptionControllerAdvice.java
+++ src/test/java/com/example/sale/practice/controller/ExceptionControllerAdvice.java
@@ -4,8 +4,13 @@ import com.example.sale.practice.dto.ResponseDTO;
 import com.example.sale.practice.exception.RequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
@@ -20,5 +25,24 @@ public class ExceptionControllerAdvice {
         }
 
         return new ResponseEntity<ResponseDTO>(response, status);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        var response = ResponseDTO.builder()
+                .code("400")
+                .message("invalid field")
+                .content(errors)
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
