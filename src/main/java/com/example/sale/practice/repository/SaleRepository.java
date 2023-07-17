765c73e272e87edf4b52ad74825f8192a6cbce94
Denis Valladares <denisvalladares402@gmail.com>
2023-07-17 15:53:31 -0600

    tag: add repository
    Client
    User
    Sale

---
+++ src/main/java/com/example/sale/practice/repository/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.example.sale.practice.repository;
+
+import com.example.sale.practice.entity.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+}
